a32c95200282dee098546d6a44f2c9b0070e1d3b
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-14 12:05:59 -0400

    adding forms

--- src/component/Forms.js
+++ src/component/Forms.js
@@ -2,21 +2,66 @@ import React, {useState} from "react";
 
 export default function Form(){
 
-    const defaultState = {
+    const [formState, setFormState] = useState ({
+
         name: "",
         email:"",
         password:"",
-        terms:""
+        notes:"",
+        positions:"",
+        terms:"" 
 
+    });
+
+    const formSubmit = e => {
+        e.preventDefault();
+        console.log ("form submitted!");
     };
 
-    const [formState, setFormState]= useState(defaultState);
+    const inputChange = e => {
+        console.log("input changed!", e.target.value);
+        setFormState({name: e.target.value});
+    };
+
+
+   
 
 
 
 
     return (
-        <form>
+        <form onSubmit = {formSubmit}>
+            <label htmlFor= "name">
+                Name
+             <input id="name" type="text" name = "name"/>
+             {/*<input id="password" type = "text" name="password"/>
+             <input id="terms" type="checkbox" name="terms"/> */}
+
+            </label>
+            <label htmlFor = "email">
+                Email
+             <input id="email" type = "email" name="email"/>
+            </label>
+            <label htmlFor = "password">
+                Password
+              <input id="password" type = "text" name="pasword"/>
+            </label>
+            <label htmlFor = "notes">
+                Is there anything you'd like to tell us?
+                <textarea name = "notes" />
+            </label>
+            <label htmlFor = "positions">
+                What would you like to help with?
+                <select id= "positions" name = "positions">
+                   <option value="teaching">Teaching</option>
+                   <option value ="assisting">Assisting</option>
+                   <option value="nightCrew">Night Crew</option>
+                   <option value="cleaning">Cleaning</option>
+                </select>
+
+            </label>
+
+            <button>Submit</button>
             
         </form>
     )
