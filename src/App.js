c61c2cc17aa733818bc8e3e7074f1c7de13157b6
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-13 22:08:46 -0400

    set up complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Forms from "./component/Forms";
+
+function App() {
+  return (
+
+    <div className = "App">
+      <h2> New User </h2>
+      <Forms />
+    </div>
+     
+  
+  );
+};
+
+export default App;
