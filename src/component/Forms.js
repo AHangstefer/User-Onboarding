90489f9626afc3f40996985e4dad74044ae14b59
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-14 20:04:11 -0400

    adding yup Schema

--- src/component/Forms.js
+++ src/component/Forms.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import * as yup from "yup";
 
 export default function Form(){
 
@@ -12,6 +13,34 @@ export default function Form(){
         terms:"" 
 
     });
+
+    {/* the keys in useState(formState) must match the "names" in 
+     inputs. When setFormState uses {name: e.target.value} it's looking at
+     the "name" key for each input. These are all connected */}
+
+    const [errors, setErrors] = useState({
+
+        name: "",
+        email:"",
+        password:"",
+        notes:"",
+        positions:"",
+        terms:"" 
+
+    });
+    
+    {/* error useState is the same as formState because we won't have
+     errors initially */}
+
+     const formSchema = yup.object().shape({
+         name: yup.string().required("Name is a required field"),
+         email: yup.string().email("Must be a valid email address").required(),
+         password: yup.string().required("Password is a required field"),
+         notes: yup.string(),
+         positions: yup.required(),
+         terms: yup.boolean().oneOf([true], "please agree to our Terms & Conditions to continue")
+     });
+
 
     const formSubmit = e => {
         e.preventDefault();
@@ -34,9 +63,7 @@ export default function Form(){
             <label htmlFor= "name">
                 Name
              <input id="name" type="text" name = "name"/>
-             {/*<input id="password" type = "text" name="password"/>
-             <input id="terms" type="checkbox" name="terms"/> */}
-
+            
             </label>
             <label htmlFor = "email">
                 Email
@@ -46,10 +73,12 @@ export default function Form(){
                 Password
               <input id="password" type = "text" name="pasword"/>
             </label>
+
             <label htmlFor = "notes">
                 Is there anything you'd like to tell us?
                 <textarea name = "notes" />
             </label>
+
             <label htmlFor = "positions">
                 What would you like to help with?
                 <select id= "positions" name = "positions">
@@ -58,10 +87,17 @@ export default function Form(){
                    <option value="nightCrew">Night Crew</option>
                    <option value="cleaning">Cleaning</option>
                 </select>
-
             </label>
 
-            <button>Submit</button>
+            <label htmlFor="terms" className="terms">
+                <input type="checkbox" name="terms" checked={true}/>
+                Terms & Conditions
+            </label>
+                
+
+        
+
+            <button type="submit">Submit</button>
             
         </form>
     )
